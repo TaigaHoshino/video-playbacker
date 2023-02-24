9e68cfb921efd4055115482c25a758eecf8ae57c
tanukis <tiger3rdstar@gmail.com>
2023-02-24 11:11:16 +0900

    ビデオリストアイテムに機能を追加

--- lib/dtos/video.dart
+++ lib/dtos/video.dart
@@ -8,6 +8,21 @@ class Video {
   DateTime createdAt;
   final String videoPath;
   final String thumbnailPath;
+  String get durationString {
+    int durationSec = videoDurationMillis ~/ 1000;
+    int hours = durationSec ~/ 3600;
+    int minutes = (durationSec % 3600) ~/ 60;
+    int seconds = durationSec % 60;
+    if(hours != 0){
+      return '$hours:$minutes:$seconds';
+    }
+    else if(minutes != 0){
+      return '$minutes:$seconds';
+    }
+    else{
+      return '$seconds';
+    }
+  }
 
   Video(this.id, this.title, this.category, this.videoDurationMillis, this.createdAt, this.videoPath, this.thumbnailPath);
 }
--- lib/screens/videolist_screen.dart
+++ lib/screens/videolist_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:video_playbacker/dtos/loading_state.dart';
@@ -21,7 +23,7 @@ class VideoListScreen extends StatelessWidget{
         StreamBuilder<LoadingState<List<Video>>>(
           stream: appBloc.videoList,
           builder: (context, snapshot){
-            Widget widget = Text("");
+            Widget widget = const Text("");
 
             if(!snapshot.hasData){
               return widget;
@@ -51,21 +53,69 @@ class VideoListScreen extends StatelessWidget{
 class VideoListItemsWidget extends StatelessWidget {
 
   final Video video;
-  String _title = "";
 
-  VideoListItemsWidget({Key? key, required this.video}): super(key: key){
-    _title = video.id.toString();
-  }
+  const VideoListItemsWidget({Key? key, required this.video}): super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return Container(child: ListTile(
-      title: Text(_title, textScaleFactor: 2),
+    return ListTile(
+      title: Text(video.title, overflow: TextOverflow.ellipsis),
+      subtitle: Text(video.createdAt.toString(), overflow: TextOverflow.ellipsis),
+      leading: Container(width:100, height: 100,
+        decoration: const BoxDecoration(color: Colors.black), child: Stack(
+        children: <Widget>[
+          Image.file(File(video.thumbnailPath)),
+          Container(
+            margin: const EdgeInsets.all(3),
+            child: 
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Text(video.durationString, style: const TextStyle(color: Colors.white, backgroundColor: Colors.black), textScaleFactor: 0.8)
+              ))
+        ]),
+      ),
+      trailing: PopupMenuButton<int>(
+          onSelected: ((value) {
+            switch(value) {
+              case 1:
+                break;
+              case 2:
+                showDialog(context: context, builder: (context) {
+                  return AlertDialog(
+                    title: const Text("確認"),
+                    content: Text('タイトル:"${video.title}"を削除します。本当にいいですか?'),
+                    actions: <Widget>[
+                      GestureDetector(
+                        child: const Text('いいえ'),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      GestureDetector(
+                        child: const Text('はい'),
+                        onTap: () {},
+                      )
+                    ],
+                  );
+                });
+                break;
+            }
+          }),
+          itemBuilder: (context) => <PopupMenuEntry<int>>[
+            const PopupMenuItem(
+              value: 1,
+              child: Text('編集')
+            ),
+            const PopupMenuItem(
+              value: 2,
+              child: Text('削除', style: TextStyle(color: Colors.red))
+            )
+          ]
+        ),
       onTap: () {
-        print(_title);
         GetIt.I<VideoPlayerHandler>().moveToTargetVideo(video.id);
         Navigator.push(context, MaterialPageRoute(builder: (context) => VideoPlayerScreen(video)));
       }
-    ));
+    );
   }
 }
