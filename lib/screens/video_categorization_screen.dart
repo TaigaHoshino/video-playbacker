45d426ab7c7e4afd3868a83638aa3196b69b5c0b
tanukis <tiger3rdstar@gmail.com>
2023-03-09 21:44:43 +0900

    ビデオカテゴリ追加画面に追加ボタンを実装

--- lib/screens/video_categorization_screen.dart
+++ lib/screens/video_categorization_screen.dart
@@ -11,8 +11,9 @@ import '../dtos/video_category.dart';
 class VideoCategorizationScreen extends StatelessWidget {
 
   final VideoCategory videoCategory;
+  final List<Video> selectedVideo = [];
 
-  const VideoCategorizationScreen({Key? key, required this.videoCategory}): super(key: key);
+  VideoCategorizationScreen({Key? key, required this.videoCategory}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,44 +23,53 @@ class VideoCategorizationScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text('${videoCategory.name}に追加'), leading: IconButton(icon: const Icon(Icons.arrow_back),onPressed: () {Navigator.pop(context);})),
-      body:
-        StreamBuilder<LoadingState<List<Video>>>(
-          stream: appBloc.videoList,
-          builder: (context, snapshot){
-            Widget widget = const Text("");
+      body:SafeArea(child: Column(children: [
+        Expanded(
+        child: StreamBuilder<LoadingState<List<Video>>>(
+            stream: appBloc.videoList,
+            builder: (context, snapshot){
+              Widget widget = const Text("");
 
-            if(!snapshot.hasData){
+              if(!snapshot.hasData){
+                return widget;
+              }
+
+              snapshot.data!.when(
+                loading: (_, __) => {},
+                completed: ((content) => {
+                  widget = ListView.builder(
+                    itemCount: content.length,
+                    itemBuilder: (context, index) {
+                      final video = content.elementAt(index);
+                      return GestureDetector(
+                        child: VideoListItemsWidget(video: video, videoCategory: videoCategory, selectedColor: Colors.blue, onTap: () {
+                          if(selectedVideo.contains(video)) {
+                            selectedVideo.remove(video);
+                          }
+                          else {
+                            selectedVideo.add(video);
+                          }
+                        }),
+                        );
+                    }
+                  )
+                }),
+                error: ((_) {
+                  
+                }));
               return widget;
             }
-
-            snapshot.data!.when(
-              loading: (_, __) => {},
-              completed: ((content) => {
-                widget = ListView.builder(
-                  itemCount: content.length,
-                  itemBuilder: (context, index) {
-                    final video = content.elementAt(index);
-                    final List<Video> selectedVideo = [];
-                    return GestureDetector(
-                      child: VideoListItemsWidget(video: video, videoCategory: videoCategory, selectedColor: Colors.blue, onTap: () {
-                        if(selectedVideo.contains(video)) {
-                          selectedVideo.remove(video);
-                        }
-                        else {
-                          selectedVideo.add(video);
-                        }
-                      }),
-                      );
-                  }
-                )
-              }),
-              error: ((_) {
-                
-              }));
-            return widget;
-          }
+          ),
+        ),
+        SizedBox(
+          height: 50,
+          child: TextButton(onPressed: () {
+              print('button pushed');
+            }, child: const Text('カテゴリに追加'))
         )
-      );
+        
+      ],)   
+      ));
   }
 
 }
